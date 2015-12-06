10e65f142c850f57d9aacfe6654becfc997e8568
AlfredYang1986 <yangyuanpig@gmail.com>
2015-12-06 17:21:22 +0800

    weixin api

--- app/controllers/JieTengService.scala
+++ app/controllers/JieTengService.scala
@@ -10,6 +10,8 @@ import controllers.common.requestArgsQuery._
 import com.mongodb.casbah.Imports._
 import java.util.Date
 import util.dao._
+import module.common.http._
+import java.security.MessageDigest
 
 /**
  * for con = 0: just upload
@@ -26,6 +28,12 @@ object queryStatus{
 
 object JieTengService extends Controller {
 
+	val app_id = "wxb46efccede9f5a76"
+	val app_secret = "06ff8eb4765422c073f555284e227a9f"
+	val weixin_http = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + app_id + "&secret=" + app_secret
+	val weixin_jsapi = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token="
+	val sign = "jsapi_ticket=#(ticket)&noncestr=Wm3WZYTPz0wzccnW&timestamp=#(time)&url=http://localhost:9000/consultingPage"
+  
 	def consultation = Action {
 		val data = Json.parse(Source.fromFile("public/data/abc-min.json").bufferedReader.readLine)
 		var lst = (data \ "data").asOpt[List[JsValue]].get
@@ -46,7 +54,28 @@ object JieTengService extends Controller {
 	}
   
 	def consultingPage(work_type: String, name: String) = Action {
-		Ok(views.html.consultingPage("Jie Teng She")(work_type)(name))
+		val wechat_token = ((HTTP(weixin_http)).get(null) \ "access_token").asOpt[String].get
+		val wechat_jsapi = ((HTTP(weixin_jsapi + wechat_token)).get(null) \ "ticket").asOpt[String].get
+		val timespan = (new Date().getTime / 1000).toString
+		val str = "jsapi_ticket=" + wechat_jsapi + "&noncestr=Wm3WZYTPz0wzccnW&timestamp=" + timespan + "&url=http://localhost:9000/consultingPage"
+		val messageDigest = MessageDigest.getInstance("SHA1");
+		messageDigest.update(str.getBytes());
+		val signiture = getFormattedText(messageDigest.digest());
+		//println(signiture)
+	  
+		Ok(views.html.consultingPage("Jie Teng She")(work_type)(name)(app_id)(signiture)(timespan))
+	}
+	
+	private def getFormattedText(bytes : Array[Byte]) : String = {
+		val sb = new StringBuffer(bytes.length)
+		var sTemp : String = ""
+		for (i <- 0 to bytes.length - 1) {
+		     sTemp = Integer.toHexString(0xFF & bytes.apply(i))
+		     if (sTemp.length() < 2) sb.append(0)
+
+		     sb.append(sTemp.toLowerCase)
+		}   
+		return sb.toString
 	}
 
 	def progress = Action {
